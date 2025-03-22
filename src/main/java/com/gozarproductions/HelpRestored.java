08aa99171bf88587d7be2160d7b25f41fecdf792
Erallie <erika@gozarproductions.com>
2025-03-22 07:41:46 -0700

    Made commands not need the slash when navigating

--- src/main/java/com/gozarproductions/HelpRestored.java
+++ src/main/java/com/gozarproductions/HelpRestored.java
@@ -98,7 +98,7 @@ public class HelpRestored extends JavaPlugin {
             }
 
             if (query != null) {
-                HelpTopicData topic = helpTopics.get(query.toLowerCase());
+                HelpTopicData topic = helpTopics.get(query.replace("/", "").toLowerCase());
                 if (topic != null && topic.canSee(sender)) {
                     List<String> content = topic.getContent();
                     int totalPages = (int) Math.ceil((double) content.size() / 9);
